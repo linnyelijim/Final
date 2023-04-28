034e5c1fa6725591fece30722f84c1c35f6c230c
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-04-28 19:43:11 -0400

    updated port. worked on requests.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
