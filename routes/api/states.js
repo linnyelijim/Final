8132c0489ae69600744cabcf24a1d86ce1ed1872
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-04-29 15:55:19 -0400

    update routes.

--- routes/api/states.js
+++ routes/api/states.js
@@ -5,26 +5,13 @@ const statesController = require('../../controllers/statesController');
 router.route('/')
     .get(statesController.getAllStates)
 
-
 router.route('/:state')
-    .get(statesController.getState);
-
-router.route('/:state/funfact')
-    .get(statesController.getFunFact)
+    .get(statesController.getState)
     .post(statesController.createFunFact)
     .patch(statesController.updateFunFact)
     .delete(statesController.deleteFunFact);
 
-router.route('/:state/capital')
-    .get(statesController.getCapital);
-
-router.route('/:state/nickname')
-    .get(statesController.getNickname);
-
-router.route('/:state/population')
-    .get(statesController.getPopulation);
-
-router.route('/:state/admission')
-    .get(statesController.getAdmission);
+router.route('/:state/:param')
+    .get(statesController.getStateProperties);
 
 module.exports = router;
